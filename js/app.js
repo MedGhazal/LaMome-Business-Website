84040980899e242853d233151f4c5444b00204cb
Mohamed Ghazal <mohamedghazal@MBP-von-Mohamed.fritz.box>
2022-02-14 20:03:13 +0100

    BUG: the language settung stays after going to another page

--- js/app.js
+++ js/app.js
@@ -14,49 +14,70 @@ tel.placeholder = data['french']['tel'];
 email.placeholder = data['french']['email'];
 message.placeholder = data['french']['message'];
 
-function onClickHandeler() {
-	for (const link of document.querySelectorAll(".language-link, .language-link-small")) {
-		link.addEventListener("click", function(event) {
-			document.querySelectorAll(".language-link, .language-link-small")
-				.forEach(el => document.querySelectorAll(
-					`.${el.getAttribute('href').substring(1)}, .${el.getAttribute('href').substring(1)}-block`)
-					.forEach(
-						el => el.style.display = 'none'
-					)
-				);
-			document.querySelectorAll(`.${event.currentTarget.getAttribute('href').substring(1)}`)
-				.forEach(
-					el => el.style.display = 'inline-block'
-			);
-			document.querySelectorAll(`.${event.currentTarget.getAttribute('href').substring(1)}-block`)
-				.forEach(
-					el => el.style.display = 'block'
-			);
-			if (`${event.currentTarget.getAttribute('href').substring(1)}` === 'fr-content') {
-				name.placeholder = data['french']['name'];
-				tel.placeholder = data['french']['tel'];
-				email.placeholder = data['french']['email'];
-				message.placeholder = data['french']['message'];
-			}
-			else if (`${event.currentTarget.getAttribute('href').substring(1)}`=== 'ar-content') {
-				name.placeholder = data['arabic']['name'];
-				tel.placeholder = data['arabic']['tel'];
-				email.placeholder = data['arabic']['email'];
-				message.placeholder = data['arabic']['message'];
-			}
-			else if (`${event.currentTarget.getAttribute('href').substring(1)}` === 'eng-content') {
-				name.placeholder = data['english']['name'];
-				tel.placeholder = data['english']['tel'];
-				email.placeholder = data['english']['email'];
-				message.placeholder = data['english']['message'];
-			}
-			document.querySelectorAll(".language-link-small").forEach(el => el.classList.remove("active"));
-			link.classList.add('active');
-		});
+function getContent(language_tag){
+	document.querySelectorAll(".language-link, .language-link-small")
+		.forEach(el => document.querySelectorAll(
+			`.${el.getAttribute('href').substring(1)}, .${el.getAttribute('href').substring(1)}-block`)
+			.forEach(
+				el => el.style.display = 'none'
+			)
+	);
+	document.querySelectorAll(`.${language_tag}`)
+		.forEach(
+			el => el.style.display = 'inline-block'
+	);
+	document.querySelectorAll(`.${language_tag}-block`)
+		.forEach(
+			el => el.style.display = 'block'
+	);
+	if (`${language_tag}` === 'fr-content') {
+		document.querySelector('header').style.width = 'min(calc(98% - 20px), 1105px)';
+		name.placeholder = data['french']['name'];
+		tel.placeholder = data['french']['tel'];
+		email.placeholder = data['french']['email'];
+		message.placeholder = data['french']['message'];
+	}
+	else if (`${language_tag}`=== 'ar-content') {
+		document.querySelector('header').style.width = 'min(calc(98% - 20px), 870px)';
+		name.placeholder = data['arabic']['name'];
+		tel.placeholder = data['arabic']['tel'];
+		email.placeholder = data['arabic']['email'];
+		message.placeholder = data['arabic']['message'];
+	}
+	else if (`${language_tag}` === 'eng-content') {
+		document.querySelector('header').style.width = 'min(clac(98% - 20px), 1020px)';
+		name.placeholder = data['english']['name'];
+		tel.placeholder = data['english']['tel'];
+		email.placeholder = data['english']['email'];
+		message.placeholder = data['english']['message'];
+	}
+	document.querySelectorAll(".add-language-suffix").forEach(
+		el => el.href = el.href.split('#')[0] + "#" + language_tag
+	)
+	document.querySelectorAll(".language-link-small").forEach(el => el.classList.remove("active"));
+	document.querySelector(`a[href='#${language_tag}']`).classList.add("active");
+}
+
+function initializePage() {
+	link = window.location.href.split('#');
+	if (link.length === 2) {
+		if (link[1] === 'fr-content' || link[1] === 'eng-content' || link[1] === 'ar-content') {
+			getContent(link[1]);
+		} else {
+			getContent('fr-content');
+		}
+	} else {
+		getContent('fr-content');
 	}
 }
 
-document.querySelector(".fr-content").style.display = "inline"
+function onClickHandeler() {
+	for (const link of document.querySelectorAll(".language-link, .language-link-small")) {
+		link.addEventListener("click", function(event) {
+			getContent(`${event.currentTarget.getAttribute('href').substring(1)}`)
+		});
+	}
+}
 
 onClickHandeler();
 var slideIndex = 1;
@@ -126,4 +147,21 @@ function toggelMenu() {
 
 
 	});
+}
+var timeout;
+function myFunction() {
+	loader_container = document.getElementById("loader-container");
+	if ( loader_container != null ) { 
+		timeout = setTimeout(showPage, 1000);
+	} else {
+		timeout = setTimeout(showPage, 0);
+	}
+}
+function showPage() {
+	loader_container = document.getElementById("loader-container");
+	if ( loader_container != null ) { 
+		loader_container.style.display = "none";
+		document.getElementById("content").style.display = "block";
+	}
+	initializePage();
 }
